81ebb6bdbeff305a07bb1f9d0a5cc2138b9b318e
Shivam Singh <roxshivamsingh@gmail.com>
2025-06-21 15:25:24 +0530

    ADDED: CSS code;

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import classes from "./App.module.css";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className={classes?.PrimaryNavigation}>
+        <ul>
+          {NAV_OPTIONS?.map((item, i) => {
+            return <li key={i}>{item.label}</li>;
+          })}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+const NAV_OPTIONS = [
+  { label: "Home", value: "home" },
+  { label: "Contact us", value: "contact us" },
+  { label: "About", value: "about" },
+  { label: "Projects", value: "projects" },
+];
